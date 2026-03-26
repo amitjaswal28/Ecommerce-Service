dfd15bb0166b56ea48a91ed5c9ff6033f0de3a2e
amitjaswal28 <amitjaswal08@gmail.com>
2026-03-26 19:43:21 +0530

    Initial project Changes

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Ecommerce.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController:ControllerBase
+    {
+        private readonly AppDbContext _DbContext;
+
+        public ProductController(AppDbContext context)
+        {
+            _DbContext = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            return Ok(_DbContext.Products.ToList());
+        }
+            
+
+    }
+}
