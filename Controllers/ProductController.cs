f4ac234a96c3e41e43fc5da9c5a695d9ba12c43a
amitjaswal28 <amitjaswal08@gmail.com>
2026-03-27 18:18:58 +0530

    CRUD API endpoints created

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -6,7 +6,7 @@ namespace Ecommerce.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProductController:ControllerBase
+    public class ProductController : ControllerBase
     {
         private readonly AppDbContext _DbContext;
 
@@ -16,11 +16,73 @@ namespace Ecommerce.API.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetProduct()
+        public async Task<IActionResult> GetProducts()
         {
-            return Ok(_DbContext.Products.ToList());
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
         }
-            
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(Product product)
+        {
+            product.CreatedAt = DateTime.Now;
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+                return NotFound();
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return Ok("Deleted");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(int id, Product updatedProduct)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                updatedProduct.Id = id;
+                updatedProduct.CreatedAt = DateTime.Now;
+
+                _context.Products.Add(updatedProduct);
+                await _context.SaveChangesAsync();
+
+                return Ok(updatedProduct);
+            }
+
+            product.Name = updatedProduct.Name;
+            product.Price = updatedProduct.Price;
+            product.Description = updatedProduct.Description;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
 
     }
 }
